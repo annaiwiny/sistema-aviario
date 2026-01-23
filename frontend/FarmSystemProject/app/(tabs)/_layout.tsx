1ba27f18467f108616563460d0d314cd919412bf
Fernando <luisfernandopassos84@gmail.com>
2026-01-23 13:14:21 -0300

    Criando aplicação

---
+++ frontend/FarmSystemProject/app/(tabs)/_layout.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Tabs } from 'expo-router';
+import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Colors } from '@/constants/theme';
+import { Platform, View } from 'react-native';
+
+function TabBarIcon(props: {
+    name: React.ComponentProps<typeof FontAwesome>['name'];
+    color: string;
+}) {
+    return <FontAwesome size={24} style={{ marginBottom: -3 }} {...props} />;
+}
+
+export default function TabLayout() {
+    const colorScheme = useColorScheme();
+
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+                
+                headerShown: false,
+            }}>
+
+        </Tabs>
+    );
+}
---
+++ frontend/FarmSystemProject/app/(tabs)/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function HomeScreen() {
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text>Teste</Text>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
