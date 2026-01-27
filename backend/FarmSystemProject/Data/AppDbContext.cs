d016bb57c181a6fe4418eb60ae5aa3c4040a9296
Antonio Higor <higor.ant02@gmail.com>
2026-01-27 14:50:12 -0300

    feat: remodelagem das entidades modelos e banco de dados. Novas migrations. Exclusão da funcionalidade de gerenciamento de raças

--- backend/FarmSystemProject/Data/AppDbContext.cs
+++ backend/FarmSystemProject/Data/AppDbContext.cs
@@ -2,6 +2,7 @@
 using FarmSystemProject.Models.HealthMonitoring;
 using FarmSystemProject.Models.NutritionalControl;
 using FarmSystemProject.Models.ProductiveMonitoring;
+using FarmSystemProject.Models.SalesRecord;
 using FarmSystemProject.Models.Users;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,17 +16,21 @@ public class AppDbContext : DbContext
     public DbSet<User> Users { get; set; }
     public DbSet<Farm> Farms { get; set; }
     public DbSet<Lot> Lots { get; set; }
-    public DbSet<Race> Races { get; set; }
     public DbSet<Mortality> Mortalities { get; set; }
     public DbSet<Vaccination> Vaccinations { get; set; }
     public DbSet<Feed> Feeds { get; set; }
     public DbSet<Feeding> Feedings { get; set; }
     public DbSet<CollectEgg> CollectEggs { get; set; }
+    public DbSet<Sale> Sales { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
         modelBuilder.Entity<Farm>().HasIndex(f => f.OwnerId).IsUnique();
         modelBuilder.Entity<Feed>().Property(f => f.BagValue).HasPrecision(18, 2);
+        modelBuilder.Entity<Sale>().Property(s => s.UnitValue).HasPrecision(18, 2);
+        modelBuilder.Entity<Sale>().Property(s => s.TotalValue).HasPrecision(18, 2);
+        modelBuilder.Entity<User>().HasIndex(u => u.Email).IsUnique();
+        modelBuilder.Entity<User>().HasIndex(u => u.Cpf).IsUnique();
     }
 }
--- backend/FarmSystemProject/Program.cs
+++ backend/FarmSystemProject/Program.cs
@@ -1,12 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using QuestPDF.Infrastructure;
 using FarmSystemProject.Data;
-using FarmSystemProject.Interfaces.IFarm;
 using FarmSystemProject.Interfaces.IProductiveMonitoring;
 using FarmSystemProject.Interfaces.IHealthMonitoring;
 using FarmSystemProject.Services.HelthMonitoringService;
 using FarmSystemProject.Services.ProductiveMonitoringService;
-using FarmSystemProject.Services.FarmService;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,7 +15,6 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(conn
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IRaceService, RaceService>();
 builder.Services.AddScoped<IEggService, EggService>();
 builder.Services.AddScoped<IMortalityService, MortalityService>();
 builder.Services.AddScoped<IVaccinationService, VaccinationService>();
