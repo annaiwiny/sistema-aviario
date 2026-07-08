ff214997200b99382a38f506e9591cc3f9d978d3
JoaoMNZ <joao.menezes21@Outlook.com>
2026-07-08 13:20:12 -0300

    Cria DTOs para registro de vendas.

--- backend/FarmSystemProject/Models/Sales/Sale.cs
+++ backend/FarmSystemProject/Models/Sales/Sale.cs
@@ -2,21 +2,28 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace FarmSystemProject.Models.SalesRecord;
+namespace FarmSystemProject.Models.Sales;
 public class Sale
 {
     [Key]
     public int Id { get; set; }
+
     [Required, Column(TypeName = "decimal(18,2)")]
     public decimal UnitValue { get; set; }
+
     [Required]
     public int EggQuantity { get; set; }
-    [Required, Column(TypeName = "decimal(18,2)")]
-    public decimal TotalValue { get; set; }
+
+    // Atributo auxiliar (Não cria coluna no banco)
+    [NotMapped]
+    public decimal TotalValue => UnitValue * EggQuantity;
+
     [Required]
     public DateTime SaleDate { get; set; }
+
     [Required]
     public int LotId { get; set; }
+
     [ForeignKey("LotId")]
-    public Lot Lot { get; set; }
+    public Lot Lot { get; set; } = null!;
 }
