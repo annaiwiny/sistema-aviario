821cc3e915749af006ce13fccdc727921e622a6e
JoaoMNZ <joao.menezes21@Outlook.com>
2026-07-09 14:57:06 -0300

    Realiza migration para a criação da tabela Feed.

--- backend/FarmSystemProject/Models/NutritionalControl/Feed.cs
+++ backend/FarmSystemProject/Models/NutritionalControl/Feed.cs
@@ -1,16 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FarmSystemProject.Models.NutritionalControl;
 public class Feed
 {
     [Key]
     public int Id { get; set; }
+
     [Required]
     public DateTime PurchaseDate { get; set; }
-    [Required]
-    public double BagWeight { get; set; }
+
+    [Required, Column(TypeName = "decimal(18,2)")]
+    public decimal BagWeight { get; set; }
+
     [Required]
     public int BagQuantity { get; set; }
-    [Required]
+
+    [Required, Column(TypeName = "decimal(18,2)")]
     public decimal BagValue { get; set; }
 }
